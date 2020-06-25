888ee7f0026bd8fdee360c7a9e76ba9596bb6e26
Tanya Hodges <tanyac52003@gmail.com>
2020-06-25 14:29:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,14 @@
 class Student
 
-
+def self.create_table
+  sql = <<-SQL
+  CREATE TABLE students(
+    id INTEGER PRIMARY KEY 
+    name TEXT,
+    grade INTEGER
+  )
+  SQL
+  DB[:conn].execute(sql)
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
 end
