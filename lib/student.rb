446c85fd281cbfbe84856fa874488f31deb15a41
Tanya Hodges <tanyac52003@gmail.com>
2020-06-25 14:49:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ def self.create_table
      INSERT INTO students(name,grade)
      VALUES(?,?)
    SQL
-   DB[:conn].execute(sql)
+   DB[:conn].execute(sql,self.name,self.grade)
    @id = DB[:conn].execute("SELECT last_inserted_rowid() FROM students")[0][0]
  end 
 end
